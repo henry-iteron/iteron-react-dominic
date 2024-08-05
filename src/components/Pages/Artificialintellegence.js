5fc72888e0f23a370555ce5257705239baad1e32
dominic-rozario <dominic.i.rozario@gmail.com>
2024-08-05 20:30:01 +0530

    update

--- src/components/Pages/Artificialintellegence.js
+++ src/components/Pages/Artificialintellegence.js
@@ -1,27 +1,235 @@
-import React from 'react'
-import "./Artificialintellegence.css"
-import { Link } from 'react-router-dom'
-
+import React from "react";
+import "./Artificialintellegence.css";
+import { Link } from "react-router-dom";
 
 export default function Artificialintellegence() {
   return (
-    <div className="pagetop-AI">
-    <div className="title-container">
-      <h1>
-        AI offerings - machine learning, deep learning,NLP, Image recognition and Computer vision
-      </h1>
-      <ul className="shortcut">
-        <li>
-          <Link className="hyper" to="/">
-            Home
-          </Link>
-        </li>
-        <li>Artificialintellegence</li>
-      </ul>
-    </div>
-  </div>
+    <>
+      <div className="pagetop-AI">
+        <div className="title-container">
+          <h1>
+            AI offerings - machine learning, deep learning,NLP, Image
+            recognition and Computer vision
+          </h1>
+          <ul className="shortcut">
+            <li>
+              <Link className="hyper" to="/">
+                Home
+              </Link>
+            </li>
+            <li>Artificialintellegence</li>
+          </ul>
+        </div>
+      </div>
 
+      <section id="offer">
+        <div className="discount">
+          <div className="offer-together">
+            <div className="offer-container">
+              <div className="offer-img">
+                <img
+                  src="https://iterontech.com/wp-content/uploads/2019/01/buttons-2-min_v1.png"
+                  alt=""
+                ></img>
+              </div>
+              <div className="ai-para">
+                <strong>AI Consultancy</strong>
+                <br></br>
+                <strong>Predictive Analysis</strong>
+                <br></br>
+                <strong>AI Developments</strong>
+                <br></br>
+                <strong>3rd Party Integration</strong>
+                <br></br>
+                <strong>Maintenance and support</strong>
+                <br></br>
+              </div>
+            </div>
 
-    
-  )
+            <div className="offer-contant">
+              <div className="offer-para">
+                <p>
+                  <strong>°&nbsp; Planning and Consulting&nbsp;</strong>AI
+                  Solutions<br></br>
+                  <strong>°&nbsp; Business Consultancy&nbsp;</strong>and
+                  Strategy<br></br>
+                  <strong>°&nbsp; Machine Learning </strong>&nbsp;Models
+                  <br></br>
+                  <strong>°&nbsp; Deep Learning </strong>Models<br></br>
+                  <strong>°&nbsp; Natural Language </strong>Processing{" "}
+                  <strong>(NLP)</strong>
+                  <br></br>
+                  <strong>°&nbsp; Solution Architecture &amp; </strong>Design
+                  <br></br>
+                  <strong>°&nbsp; Predictive Analysis </strong>and Modelling
+                  <br></br>
+                  <strong>° Image and Voice Processing</strong>
+                  <br></br>
+                  °&nbsp; Experts in&nbsp;<strong>Python,R, Open MV</strong>
+                  <br></br>
+                  <strong>°&nbsp; </strong>Advisory on choosing the
+                  <strong>right platform</strong> for the specified use case and
+                  industry<br></br>
+                  <strong>°&nbsp; </strong>Monitoring and&nbsp;
+                  <strong>Support</strong>
+                  <br></br>
+                  <strong>°&nbsp; </strong>Developing&nbsp;
+                  <strong>proof-of-concepts</strong>&nbsp;for used cases in AI
+                  <br></br>
+                  <b>°&nbsp; Anomaly&nbsp;detection</b>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="ai-logo-box">
+        <div className="ai-logo">
+          <div className="ai-logo-cont">
+            <div className="ai-logo-1">
+              <img
+                src="https://www.iterontech.com/wp-content/uploads/2019/06/python-logo-png-newpythonlogo-png-1024-300x146.png"
+                alt=""
+              ></img>
+            </div>
+            <div className="ai-logo-1">
+              <img
+                src="https://www.iterontech.com/wp-content/uploads/2019/06/image7-1-300x109.jpg"
+                alt=""
+              ></img>
+            </div>
+          </div>
+          <div className="ai-logo-cont">
+            <div className="ai-logo-1">
+              <img
+                src="https://www.iterontech.com/wp-content/uploads/2019/06/openmv-logosmall-ROBU.IN_-3-300x152.png"
+                alt=""
+              ></img>
+            </div>
+            <div className="ai-logo-1">
+              <img
+                src="	https://www.iterontech.com/wp-content/uploads/2019/06/Tensorflow-Inception-300x127.png"
+                alt=""
+              ></img>
+            </div>
+          </div>
+          <div className="ai-logo-cont">
+            <div className="ai-logo-1">
+              <img
+                src="	https://www.iterontech.com/wp-content/uploads/2019/06/d9ef31e261aab6d826028e79a11c80da-300x129.png"
+                alt=""
+              ></img>
+            </div>
+            <div className="ai-logo-1">
+              <img
+                src="https://www.iterontech.com/wp-content/uploads/2019/06/1_S8Il5ethl3YFh0M9XKVz-A-2-300x129.png"
+                alt=""
+              ></img>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="total-count">
+        <div className="count-box">
+          <div className="storage">
+            <div className="count">
+              <div className="satis-client">
+                <div className="satis-img">
+                  <img
+                    src="https://www.iterontech.com/wp-content/uploads/2017/01/counter-icon1.png"
+                    alt=""
+                  ></img>
+                </div>
+                <div className="count-contant">
+                  <strong className="count-number">6</strong>
+                  <br></br>
+                  <span className="count-name">Statisfied Clients</span>
+                </div>
+              </div>
+              <div className="satis-client">
+                <div className="satis-img">
+                  <img
+                    src="	https://www.iterontech.com/wp-content/uploads/2017/01/counter-icon2.png"
+                    alt=""
+                  ></img>
+                </div>
+                <div className="count-contant">
+                  <strong className="count-number">10</strong>
+                  <br></br>
+                  <span className="count-name">AI Consultants</span>
+                </div>
+              </div>
+              <div className="satis-client">
+                <div className="satis-img">
+                  <img
+                    src="	https://www.iterontech.com/wp-content/uploads/2017/01/counter-icon4.png"
+                    alt=""
+                  ></img>
+                </div>
+                <div className="count-contant">
+                  <strong className="count-number">10</strong>
+                  <br></br>
+                  <span className="count-name">AI Projects</span>
+                </div>
+              </div>
+              <div className="satis-client">
+                <div className="satis-img">
+                  <img
+                    src="https://www.iterontech.com/wp-content/uploads/2017/01/counter-icon1.png"
+                    alt=""
+                  ></img>
+                </div>
+                <div className="count-contant">
+                  <strong className="count-number">3</strong>
+                  <br></br>
+                  <span className="count-name">AI Products</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="introduction-ai">
+        <div className="ai-container">
+          <p>
+            We are experts in providing AI Solutions for various Industries like
+            digital, real estate, advertising and retail. Read through our
+            solutions and contact us for the demo.
+          </p>
+          <br></br>
+          <p>
+            We build customized solutions for our Customers using Python, R,
+            OpenMV, Keras, Tensor flow etc. An AI Solution needs to be
+            integrated and be fit in the existing IT Landscape &amp;
+            Architecture and this is exactly what we are specialized in offering
+            an end to end packaged AI solution.
+          </p>
+        </div>
+      </section>
+
+      <section className="ai-offers">
+        <div className="ai-offer-cont">
+          <div className="ai-box">
+            <div className="ai-name">
+              <h6>Image recognition for zoo & Museum</h6>
+            </div>
+            <div className="ai-paragraph">
+              <p>
+                Our AI apps provides an Integrated solutions for zoo and Museums
+                to attract visitors{" "}
+              </p>
+            </div>
+            <div className="ai-offer-link">
+              <Link className="AI-link" to="/Artificialintellegence">
+                <i className="book fa-solid fa-book"></i> Read more
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
